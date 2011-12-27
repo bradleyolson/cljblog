5f0f9ecf799c3960c0215797325e89c45ca1d608
Brad Olson <alllateralus@gmail.com>
2011-12-27 12:01:20 -0600

    removed CLOJURE_ from db

--- src/models/blog.clj
+++ src/models/blog.clj
@@ -9,25 +9,25 @@
 
 (defn retrieve-all
   []
-  (sql/with-connection (System/getenv "CLJBLOG_DATABASE_URL")
+  (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/with-query-results results
       ["select * from blog order by id desc"]
       (into [] results))))
 
 (defn retrieve
   [& limit]
-  (sql/with-connection (System/getenv "CLJBLOG_DATABASE_URL")
+  (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/with-query-results results
       [(str "select * from blog order by id desc" (str " limit " limit))]
       (into [] results))))
 
 (defn retrieve-with
   [& args]
-  (sql/with-connection (System/getenv "CLJBLOG_DATABASE_URL")
+  (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/with-query-results results
       [(str "select * from blog" (apply str " " (interpose " " args)))]
       (into [] results))))
 
 (defn create [shout]
-  (sql/with-connection (System/getenv "CLJBLOG_DATABASE_URL")
+  (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/insert-values :shouts [:body] [shout])))
